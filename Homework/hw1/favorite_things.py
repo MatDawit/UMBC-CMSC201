848dc24369ab3eddb701840858c49cb425a74608
MatDawit <51540426+MatDawit@users.noreply.github.com>
2024-12-17 18:21:54 -0500

    Create favorite_things.py

---
+++ Homework/hw1/favorite_things.py
@@ -0,0 +1,13 @@
+"""
+File:    favorite_things.py
+Author:  Mathew Dawit
+Date:    09/08/24
+Section: 74
+E-mail:  mdawit1@umbc.edu
+Description:
+  Outputs program creator favorite food, movie, and music.
+"""
+
+print("My favorite food is chicken.")
+print("My favorite movie is The Black Phone.")
+print("My favorite music is RNB.")
